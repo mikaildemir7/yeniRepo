14526053164b12429615e26e3448775b56a4f261
Mikail Demir <demirmiko7@gmail.com>
2023-05-06 22:17:05 +0300

    V2

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -2,9 +2,7 @@ public class Class01 {
     public static void main(String[] args) {
 
         //V1 çalışmalarım...
-        //V2 üzerine çalışmalar..
-        System.out.println("V3 kodları..."); //V3
-        //V4 kısa yoldan commit
+        //V2 kodları...
 
 
 
